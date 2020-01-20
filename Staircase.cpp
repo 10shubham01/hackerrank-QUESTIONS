fccf278feac4dc5f25f0556a5df7be1af9eb524b
Shubham Gupta <47596910+10shubham01@users.noreply.github.com>
2020-01-20 17:51:51 +0530

    Create Staircase.cpp

---
+++ Staircase.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+    int main()
+    {
+        int a[100],i,n,j;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            for(j=i;j<n;j++)
+                {cout<<" ";}
+
+            for(j=1;j<=i;j++)
+                {cout<<"#";}
+                cout<<endl;
+
+        }}
+
